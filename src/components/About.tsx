9bdd21b124545e606334e0b46dfa836c048d66e2
iamValen <valentimk04@gmail.com>
2026-07-04 18:26:27 +0100

    feat: about section

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import { Section } from './Section'
+
+export function About() {
+  return (
+    <Section id="about" title="About">
+      <p>
+        <p>
+          Learned a lot by sticking with my own projects, and doing them when I didn't know what to start. 
+          I try to be always building something, either for fun or to learn something new, coding or having new ideas. 
+        </p>
+      </p>
+      <p className="muted">
+        I will join the Deloitte team with a lot to learn but very excited to join the market. I am looking forward to the new challenges and opportunities that await me in this role.
+      </p>
+    </Section>
+  )
+}
