515d70875cf8e2bdcae1ce9fb0b91b83f45142a7
iamValen <valentimk04@gmail.com>
2026-07-07 21:41:54 +0100

    feat: home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { Hero } from '../components/Hero'
+import { About } from '../components/About'
+import { Experience } from '../components/Experience'
+import { Projects } from '../components/Projects'
+import { HomeLab } from '../components/HomeLab'
+import { Stack } from '../components/Stack'
+import { Education } from '../components/Education'
+
+export function Home() {
+  return (
+    <>
+      <Hero />
+      <hr className="divider" />
+      <About />
+      <Experience />
+      <Projects />
+      <HomeLab />
+      <Stack />
+      <Education />
+    </>
+  )
+}
